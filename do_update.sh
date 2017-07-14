8fa2a70df844daa8310dc93033e960563898d99c
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-07-14 10:26:12 +1000

    drop ghc-7.8

--- do_update.sh
+++ do_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -f update_template.sed template/README.md.template       > README.md
-sed -f update_template.sed template/stack-7.8.yaml.template  > stack-7.8.yaml
 sed -f update_template.sed template/stack-7.10.yaml.template > stack-7.10.yaml
 sed -f update_template.sed template/stack-8.0.yaml.template  > stack-8.0.yaml
 
