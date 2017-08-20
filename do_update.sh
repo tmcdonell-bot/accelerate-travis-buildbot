48e21ceb265af1e96a47ddd1658022d442ca9149
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-08-20 16:07:33 +1000

    add travis/stack config for ghc-8.2

--- do_update.sh
+++ do_update.sh
@@ -3,4 +3,5 @@
 sed -f update_template.sed template/README.md.template       > README.md
 sed -f update_template.sed template/stack-7.10.yaml.template > stack-7.10.yaml
 sed -f update_template.sed template/stack-8.0.yaml.template  > stack-8.0.yaml
+sed -f update_template.sed template/stack-8.2.yaml.template  > stack-8.2.yaml
 
