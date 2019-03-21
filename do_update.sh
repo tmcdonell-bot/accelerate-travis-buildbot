0862e7a60731a49246f3710d5d28a943b3c43ebd
Trevor L. McDonell <trevor.mcdonell@gmail.com>
2019-03-21 17:21:54 +0100

    update build configuration matrix

--- do_update.sh
+++ do_update.sh
@@ -6,4 +6,5 @@ sed -f update_template.sed template/README.md.template       > README.md
 sed -f update_template.sed template/stack-8.0.yaml.template  > stack-8.0.yaml
 sed -f update_template.sed template/stack-8.2.yaml.template  > stack-8.2.yaml
 sed -f update_template.sed template/stack-8.4.yaml.template  > stack-8.4.yaml
+sed -f update_template.sed template/stack-8.6.yaml.template  > stack-8.6.yaml
 
